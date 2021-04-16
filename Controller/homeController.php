a8ea8ae5a5018e22d1c232065a84a9e130e1a8a8
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-04-15 22:35:44 -0300

    Refazendo Controller/Home, Model/Postagem para fixação do Twig

--- Controller/homeController.php
+++ Controller/homeController.php
@@ -5,25 +5,39 @@ require_once "Model/Postagem.php";
 
 Class HomeController
 {
-    public function index(){
-        
-        try {
-            $postagem = Postagem::listarTodasPostagem(); // Chama o metodo 
+    public function index()
+    {
+    
+       try {
+            $postagem = Postagem::listarTodasPostagem();
+            /*print_r($postagem);
+            exit;*/
 
-            $loader = new \Twig\Loader\FilesystemLoader('View'); // Faz um autoload do template
-            $twig = new \Twig\Environment($loader); // Carrega a varial $loader
-            $template = $twig->load('home.html'); // Carrega a pagina dentro do template
+            $loader = new \Twig\Loader\FilesystemLoader('View'); // Faz um autoload do Template
+            $twig = new \Twig\Environment($loader); // Carrega o diretorio do template
+            $template = $twig->load('home.html'); //  Carrega a pagina dentro do template
 
             $parametro = array();
-            $parametro['post'] = $postagem;
-
-            $conteudo = $template->render($parametro);
-            print_r($conteudo);
+            $parametro['post'] = $postagem; // Armazena os dados vindo do banco de dados
             
-        } catch (Exception $e) {
-            echo "Erro -> ".$e->getMessage();
-        }
-       
+            $conteudo = $template->render($parametro); 
+            print_r($conteudo);
+
+       } catch (Exception $e) {
+           echo"Erro -> ".$e->getMessage();
+       }
+
+    }
+
+    public function cadastrar(){
+        
+        $loader = new \Twig\Loader\FilesystemLoader('View');
+        $twig = new \Twig\Environment($loader);
+        $template = $twig->load('cadastrar.html');
+
+        $parametro ['teste']= 'teste2';
+        $conteudo = $template->render($parametro);
+        print_r($conteudo);
     }
 }
 ?>
--- Model/Postagem.php
+++ Model/Postagem.php
@@ -1,19 +1,26 @@
 <?php
 
-class Postagem {
+class Postagem 
+{
  
     public static function listarTodasPostagem()
     {
-      $sql = "SELECT * FROM postagem ORDER BY 1 DESC";
-      $con = Conexao::getInstance()->prepare($sql);
-      $con->execute();
+        $sql = "SELECT * FROM postagem ORDER BY id DESC";
+        $con = Conexao::getInstance()->prepare($sql);
+        $con->execute();
 
-      $resultado = array();
-
-      while ($row = $con->fetchObject('Postagem')) {
-          $resultado[] = $row;
-      }
-     return $resultado;
+        if ($con->rowCount() >= 1) {
+        
+        $resultado = array();
+        
+        while ($row = $con->fetchObject('Postagem')) 
+        {
+            $resultado[] = $row;
+        }
+        return $resultado;
+        } else {
+            throw new Exception("Ocorreu algum erro!");
+        }
     }
 }
 
